c416e0273b0e6e94e2a86cfa6be8c5c2d9b1edb2
Michal Front <michal.front@outlook.com>
2022-01-31 19:04:05 +0100

    Final Touches

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 
-import InputForm from './layouts/InputForm';
+import InputForm from './components/FeeForm';
 import Header from 'components/Header';
 
 function App() {
